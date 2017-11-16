61655561788fca3c3dd8fa0f4d75a374e49485c6
Aleksey <amstarchenko@edu.hse.ru>
2017-11-16 16:43:11 +0300

    hw4

---
+++ hw4/hw.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+'''Вариант 3'''
+
+def main():
+    filename = input(
+    'Введите полное название файла (с путём к нему) или введите '
+    'название файла, лежащего в одной папке с программой: ')
+
+    filename = filename.replace('\\', '/')
+
+    with open(filename) as f:
+        text = f.read()
+
+    punctuation = '''!"#$%&'()*+,./:;<=>?@[\]^_`{|}~'''
+
+    for c in punctuation:
+        text = text.replace(c, '')
+
+    words = text.split()
+
+    c_one = 0
+    c_three = 0
+
+
+
+    for word in words:
+        if len(word) == 1 and word not in '—–−-':
+            c_one +=1
+        elif len(word) == 3:
+            c_three += 1
+
+    if c_one == 0:
+        print('Нет слов длины 1')
+    else:
+        x = c_three/c_one
+        print('Слов длины 3 в', x, 'раз больше, чем слов длины 1.')
+
+try:
+    main()
+except Exception:
+    print('Что-то пошло не так... Возможно, неправильно указано имя файла',
+    'или путь к нему.')
