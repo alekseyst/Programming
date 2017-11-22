a0c26317b8f565db2c85bfa7243eb07c38d52165
Aleksey <amstarchenko@edu.hse.ru>
2017-11-22 21:25:26 +0300

    hw5

---
+++ hw5/hw.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+'''Вариант 3'''
+
+inp = 'a'
+coun = 0
+
+with open('out.txt', 'w') as f:
+    while inp:
+        coun += 1
+        inp = input('Введите слово: ')
+        f.write(inp[coun:] + '\n')
